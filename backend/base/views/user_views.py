9bc8e4605ba131adc1e1b818944b58ca5370da61
PH <phpcareli@gmail.com>
2022-09-05 22:30:29 -0300

    FIrst commit

---
+++ backend/base/views/user_views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from django.contrib.auth.models import User
+from base.products import products
+from base.models import Product, User
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+
+        for key, value in serializer.items():
+            print(data[key])
+            data[key] = value
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['Post'])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()  # return everything from the db
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+// function App() {
+//     return (
+//         <div>
+//             <Header />
+//             <main className="py-3">
+//                 <Container>
+//                     <HomeScreen />
+//                 </Container>
+//             </main>
+
+//             <Footer />
+//         </div>
+//     );
+// }
+
+// export default App;
+
+function App() {
+    return (
+        <Router>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    {/* <HomeScreen /> */}
+                    <Routes>
+                        <Route path="/" element={<HomeScreen />} />
+                        <Route path="/login" element={<LoginScreen />} />
+                        <Route path="/product/:id" element={<ProductScreen />} />
+                        <Route path="/cart/:id" element={<CartScreen />} />
+                        <Route path="/cart" element={<CartScreen />} />
+                    </Routes>
+                </Container>
+            </main>
+            <Footer />
+        </Router>
+    );
+}
+
+export default App;

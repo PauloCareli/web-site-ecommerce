79f9a400312ef47585ed8ba0abc31799233b9557
Paulo Careli <paulo@rvnatech.com>
2022-09-14 22:49:55 -0300

    Minor update

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -73,7 +73,7 @@ class OrderSerializer(serializers.ModelSerializer):
     def get_shippingAddress(self, obj):
         try:
             address = ShippingAddressSerializer(
-                obj.shippingAddress, many=False)
+                obj.shippingaddress, many=False).data
         except:
             address = False
         return address
